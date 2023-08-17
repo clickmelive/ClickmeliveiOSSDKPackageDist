bcd03dc24436656c7b459e571da799c5749da3e5
Salih Sevgican <sevgicansalih@gmail.com>
2023-08-17 21:22:31 +0400

    package swift tools 5.4 downgrade and lottie version fixing

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -19,7 +19,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.6.4")),
         .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.0"),
-        .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.2.0")
+        .package(url: "https://github.com/airbnb/lottie-spm.git", Version("4.2.0")...Version("4.2.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
